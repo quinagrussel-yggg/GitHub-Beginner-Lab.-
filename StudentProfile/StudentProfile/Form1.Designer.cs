b7b05ba9d4c186cdc31f6fd5fbca3f89f7ce30ef
Russel Quinag <quinagrussel@gmail,com>
2026-09-14 20:47:34 +0800

    Add student contact number.

--- StudentProfile/StudentProfile/Form1.Designer.cs
+++ StudentProfile/StudentProfile/Form1.Designer.cs
@@ -29,23 +29,35 @@
         private void InitializeComponent()
         {
             label1 = new Label();
+            label2 = new Label();
             SuspendLayout();
             // 
             // label1
             // 
             label1.AutoSize = true;
             label1.Font = new Font("Segoe UI", 13.8F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            label1.Location = new Point(185, 184);
+            label1.Location = new Point(189, 76);
             label1.Name = "label1";
             label1.Size = new Size(412, 31);
             label1.TabIndex = 0;
             label1.Text = "Student Profile — GitHub Beginner Lab";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 13.8F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label2.Location = new Point(247, 211);
+            label2.Name = "label2";
+            label2.Size = new Size(331, 31);
+            label2.TabIndex = 1;
+            label2.Text = "Contact Number: 09171234567.";
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(label2);
             Controls.Add(label1);
             Name = "Form1";
             Text = "Form1";
@@ -56,5 +68,6 @@
         #endregion
 
         private Label label1;
+        private Label label2;
     }
 }
